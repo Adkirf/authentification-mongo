0ee156e6ca29da89913cbb6e28727ccebd408198
youba-experiences <social@youba.io>
2024-05-13 16:41:12 +0100

    automatically update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,5 @@
 self.addEventListener("install", function (event) {
+  self.skipWaiting(); // Add this to skip the waiting phase and activate the new service worker as soon as it's finished installing
   event.waitUntil(
     caches.open("v1").then(function (cache) {
       return cache.addAll([
@@ -13,6 +14,12 @@ self.addEventListener("install", function (event) {
   );
 });
 
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    clients.claim() // This will take control of all clients under the service worker's scope immediately after it activates
+  );
+});
+
 self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
@@ -20,3 +27,10 @@ self.addEventListener("fetch", function (event) {
     })
   );
 });
+
+// Listen for the controllerchange event on the client side
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.addEventListener("controllerchange", () => {
+    window.location.reload(); // Reload the page to load the new version
+  });
+}
